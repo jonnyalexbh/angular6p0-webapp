859edd4548e99b743adaee9a89ffcb6e6201b620
jonnyalexbh <jonnyalex.bh@gmail.com>
2018-05-20 14:12:07 -0500

    ES6 global configuration file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
-
+import { GLOBAL } from './services/global';
+/**
+* decorator
+*
+*/
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+/**
+* AppComponent
+*
+*/
 export class AppComponent {
+
   title = 'Products Angular 6';
+  public header_color: string;
+  /**
+  * constructor
+  *
+  */
+  constructor() {
+    this.header_color = GLOBAL.header_color;
+    console.log(this.header_color)
+  }
 }
---
+++ src/app/services/global.ts
@@ -0,0 +1,4 @@
+export var GLOBAL = {
+  url: 'http://localhost:92/index.php/',
+  header_color: '#E03137'
+};
